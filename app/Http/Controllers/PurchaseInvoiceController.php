3e4129e2c61be402ce923af825ade3f5f24caad3
rpevillamonte <58614550+rpevillamonte@users.noreply.github.com>
2021-04-06 11:22:05 +0800

    Started on functions for purchase invoice

--- app/Http/Controllers/PurchaseInvoiceController.php
+++ app/Http/Controllers/PurchaseInvoiceController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MaterialPurchased;
 use App\Models\PurchaseInvoice;
 use Illuminate\Http\Request;
 use DB;
@@ -13,5 +14,11 @@ class PurchaseInvoiceController extends Controller
     public function index() {
         //$purchase_invoice = PurchaseInvoice::all();
         return view('modules.buying.purchaseinvoice');
+    }
+
+    public function openInvoiceForm() {
+        $orders = MaterialPurchased::where('mp_status', 'To Receive and Bill')->get();
+        //also add purchase receipts when created
+        return view('modules.buying.newPurchaseInvoice', ['orders' => $orders]);
     }
 }
