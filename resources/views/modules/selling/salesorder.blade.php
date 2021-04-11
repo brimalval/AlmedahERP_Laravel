7a5426fe34d9ff01a7d6ef76ce012972422e1e68
demyu <48234301+demyu@users.noreply.github.com>
2021-04-12 00:34:09 +0800

    Removed ultimateeComponentTable. Might be unnecessary

--- resources/views/modules/selling/salesorder.blade.php
+++ resources/views/modules/selling/salesorder.blade.php
@@ -788,7 +788,7 @@ $("#sales_order_form").submit(function(e) {
     var formData = new FormData(this);
     console.log(currentCart);
     formData.append("cart",currentCart );
-    formData.append("component", JSON.stringify(ultimateComponentTable));
+    //formData.append("component", JSON.stringify(ultimateComponentTable));
     formData.append("installmentType", document.getElementById('installmentType').value);
     $.ajax({
         type: 'POST',
@@ -801,7 +801,6 @@ $("#sales_order_form").submit(function(e) {
             $('#saveSaleOrder1').click(function() {
                 $('#newSalePrompt').modal('hide');
             });
-            formReset();
             document.getElementById('closeSaleOrderModal').click();
             console.log(data);
             loadRefresh();
@@ -850,6 +849,7 @@ function loadRefresh(){
                 </tr>`
                ]).draw(false);
             });
+            formReset();
         }
     });
 }
