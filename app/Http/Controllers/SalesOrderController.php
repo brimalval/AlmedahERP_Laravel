2ed1e962b4119924c02785997932318acd1f7d7b
demyu <48234301+demyu@users.noreply.github.com>
2021-04-17 11:02:41 +0800

    Fixed Add payment not setting status to fully paid

--- app/Http/Controllers/SalesOrderController.php
+++ app/Http/Controllers/SalesOrderController.php
@@ -353,7 +353,7 @@ class SalesOrderController extends Controller
         $data->payment_balance = $payment['payment_balance'] - $form_data['view_totalamount'];
 
         if($data->payment_balance <= 0.00){
-            $data->sales_status = "Fully Paid";
+            $sales->sales_status = "Fully Paid";
         }else{
             $sales->sales_status = "With Outstanding Balance";
         }
