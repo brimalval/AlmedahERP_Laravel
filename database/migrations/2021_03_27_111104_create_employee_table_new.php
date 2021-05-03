310a1fb8b1218fa3cc32e1002613c986c61fca1b
marc <marc@delacruz.com>
2021-05-03 20:23:36 +0800

    updated component and added employee

---
+++ database/migrations/2021_03_27_111104_create_employee_table_new.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeeTableNew extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('env_employees');
+        Schema::create('env_employees', function (Blueprint $table) {
+            $table->string('employee_id')->unique();
+            $table->string('last_name');
+            $table->string('first_name');
+            $table->string('position');
+            $table->enum('gender', array('Male', 'Female'));
+            $table->string('contact_number');
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employee_table_new');
+    }
+}
