22fe6cb00c01cf7a42bbc1bfcc3e37e50fab1734
Alexander Jody Dela Cruz <qajldelacruz@tip.edu.ph>
2021-05-15 10:28:10 +0800

    Work Center
    Duration to Hours, Minutes, Seconds

--- public/js/newWorkcenter.js
+++ public/js/newWorkcenter.js
@@ -28,20 +28,24 @@ function showForm() {
     let lastRow = $('#newemployee-input-rows tr:last');
     let nextID = (lastRow.length != 0) ? lastRow.data('id') + 1 : 0;
     $('#newemployee-input-rows').append(
-    `                <tr class="text-muted">
+    ` <tr class="text-muted">
     <td class="text-center">Employee Name</td>
-    <td class="text-center">Duration</td>
+    <td class="text-center">Hours</td>
+    <td class="text-center">Minutes</td>
+    <td class="text-center">Seconds</td>
     <td></td>
   </tr>
 </thead>
-<tbody class="" id="newrouting-input-rows">
+<tbody class="" id="newemployee-input-rows">
 <tr data-id="${nextID}">
  <td id="mr-code-input" class="mr-code-input"><input type="text" value=""   name="Employee_name" id="Employee_name" class="form-control"></td>
- <td style="width: 10%;" class="mr-qty-input"><input type="text" value=""  name="duration" id="duration" class="form-control">
-<td>
-<a id="" class="btn delete-btn" href="#" role="button">
-<i class="fa fa-trash" aria-hidden="true"></i>
-</a>
-</td>
+ <td style="width: 10%;" class="mr-qty-input"><input type="number" min="0" value=""  name="Employee_hours" id="Employee_hours" class="form-control"></td>
+ <td style="width: 10%;" class="mr-qty-input"><input type="number" min="0" value=""  name="Employee_minutes" id="Employee_minutes" class="form-control"></td>
+ <td style="width: 10%;" class="mr-qty-input"><input type="number" min="0" value=""  name="Employee_seconds" id="Employee_seconds" class="form-control"></td>
+ <td>
+ <a id="" class="btn delete-btn" href="#" role="button">
+ <i class="fa fa-trash" aria-hidden="true"></i>
+ </a>
+ </td>
 </tr>`);
 }
