ab52dd8bd5422c81ee2eb9c4b1189cd16093cd8a
rpevillamonte <58614550+rpevillamonte@users.noreply.github.com>
2021-08-04 09:53:53 +0800

    Revert "Revert "Revert "Merge https://github.com/databaseteam-almedah/AlmedahERP_Laravel into main"""
    This reverts commit 3124f50bd7e92120cb8305d072469df2a9a58e51.

--- resources/views/modules/reports/reports_materials_purchased.blade.php
+++ resources/views/modules/reports/reports_materials_purchased.blade.php
@@ -1,60 +1,63 @@
 <link rel="stylesheet" href="{{ asset('css/reports.css') }}">
 
 <div class="container-fluid h-50">
-    <div class="row mt-2 mb-3 h-100">
-        <div class="col-12">
-            <div class="card h-100">
+        <div class="row mt-2 mb-3 h-100">
+            <div class="col-12">
+                <div class="card h-100">
                     <div class="card-header">
                         <div class="col-lg-12">
                         <h2 class="navbar-brand tab-list-title">
                               <span>Purchase Order Report</span>
                         </h2>
-                        <div align="center" class="col-md-12" id="chart-pie-div"></div>  
+                         <div align="center" class="col-md-12" id="chart-pie-div"></div>  
+                 </div>
             </div>
         </div>
     </div>
 </div>
  
-
-<div class="col-12">
-    <div class="card h-100">
-         <div class="card-header">
-               <div class="col-lg-12">
+<div class="row mt-2 mb-3 h-100">
+    <div class="col-12">
+        <div class="card h-100">
+            <div class="card-header">
+                <div class="col-lg-12">
                     <div class="row">
                         <div id="fixed" class="col-md-12">
-                             <div class="d-flex flex-row-reverse">
-                                 <select data-column="0" id='mp_status' class="form-control flex-row-reverse"style="width: 200px" method="POST">
+                            <div class="d-flex flex-row-reverse">
+                                <select data-column="0" id='mp_status' class="form-control flex-row-reverse"style="width: 200px" method="POST">
+                            
                                     <option value="">[ By Status ]</option>
                                     @foreach($mp_status as $mp_status)
                                     <option value="{{$mp_status}}">{{$mp_status}}</option>
                                     @endforeach
                                 </select>
-                             </div>    
-                             <br>
-         <table class="table table-bordered" id="mp_charts_table">
-            <thead>
-                <th>ID</th>
-                <th>Purchase ID</th>
-                <th>Supplier Quatation ID</th>
-                <th>Purchase Date</th>
-                <th>Total Cost</th>
-                <th>Status</th>
-            </thead>
-            <tbody>
-         
-                    @foreach($materials_purchasedDataTable as $value)
-                        <tr>
-                            <td>{{$value->id}}</td>
-                            <td>{{$value->purchase_id}}</td>
-                            <td>{{$value->supp_quotation_id}}</td>
-                            <td>{{date('F d, Y',strtotime($value->purchase_date))}}</td>
-                            <td>₱{{number_format($value->total_cost,2)}}</td>
-                            <td>{{($value->mp_status)}}</td>
-                        </tr>
-                    @endforeach
+                            </div>    
+                            <br>
+                            <table class="table table-bordered" id="mp_charts_table">
+                                <thead>
+                                    <th>ID</th>
+                                    <th>Purchase ID</th>
+                                    <th>Supplier</th>
+                                    <th>Purchase Date</th>
+                                    <th>Total Cost</th>
+                                    <th>Status</th>
+                                </thead>
+                                <tbody>
+                            
+                                    @foreach($materials_purchasedDataTable as $value)
+                                        <tr>
+                                            <td>{{$value->id}}</td>
+                                            <td>{{$value->purchase_id}}</td>
+                                            <td>{{$value->supp_quotation_id}}</td>
+                                            <td>{{date('F d, Y',strtotime($value->purchase_date))}}</td>
+                                            <td>₱{{number_format($value->total_cost,2)}}</td>
+                                            <td>{{($value->mp_status)}}</td>
+                                        </tr>
+                                    @endforeach
 
-            </tbody>
-        </table>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -63,4 +66,3 @@
 </div>
 {{--FOR RENDERING OF CHART--}}
 {!! \Lava::render('PieChart', 'pie-chart', 'chart-pie-div') !!}
-
--- resources/views/modules/reports/reports_purchase_and_sales.blade.php
+++ resources/views/modules/reports/reports_purchase_and_sales.blade.php
@@ -1,5 +1,6 @@
 <link rel="stylesheet" href="{{ asset('css/reports.css') }}">
 
+@if(!empty($blogs)) <h1>hello</h1> @endif
 
 <div class="container-fluid h-50">
 <h2 class="navbar-brand tab-list-title">
@@ -41,7 +42,7 @@
                   <div class="icon text-white bg-blue"><i class="fa fa-dolly-flatbed"></i></div>
                 </div>
               </div>
-             <!-- <div class="col-xl-3 col-lg-6 mb-4 mb-xl-0">
+              <div class="col-xl-3 col-lg-6 mb-4 mb-xl-0">
                 <div class="card-header bg-white rounded  p-4 h-100 d-flex align-items-center justify-content-between">
                   <div class="flex-grow-1 d-flex align-items-center">
                     <div class="dot mr-3 bg-red"></div>
@@ -51,7 +52,7 @@
                   </div>
                   <div class="icon text-white bg-red"><i class="fas fa-receipt"></i></div>
                 </div>
-              </div>-->
+              </div>
             </div>
           </section>  
   
@@ -71,25 +72,7 @@
                 <div class="card h-100">
                     <div class="card-header">
                         <div class="col-lg-12">
-                        <table class="table table-bordered datatable" id="mp_charts_table">
-                    <thead>
-                        <th>Raw Material</th>
-                        <th>Purchase Date</th>
-                        <th>Total Cost</th>
-                    </thead>
-                    <tbody>
 
-                            @foreach($rawmats1 as $value)
-                                <tr>
-                                    <td>{{$value->item_name}}</td>
-                                    <td>{{$value->rm_date}}</td>
-                                    <td>₱{{number_format($value->sums,2)}}</td>
-
-                                </tr>
-                            @endforeach
-
-                    </tbody>
-                </table>
             </div>
         </div>
     </div>
@@ -97,6 +80,6 @@
 </div>
 </div>
 </div>
-</div>
+
 {{--FOR RENDERING OF CHART--}}
 {!! \Lava::render('ColumnChart', 'column-chart', 'chart-pie-div') !!}
